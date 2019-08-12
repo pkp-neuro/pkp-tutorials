3a5b07166e4ca278149a41ac902a9392dec50561
hennequin <gje.hennequin@gmail.com>
2019-08-12 14:25:44 +0100

    visual coding tutorial

--- src/visual_coding.ml
+++ src/visual_coding.ml
@@ -1,151 +1,248 @@
-(** Visual coding: dense vs sparse coding *)
-
 open Owl
 
-let minibatch file patch_size =
-  let images = Arr.load file in
-  let n_img, dim_x, dim_y =
-    let s = Arr.shape images in
-    s.(0), s.(1), s.(2)
-  in
-  fun batch_size ->
-    Array.init batch_size (fun _ ->
-        let i = Random.int n_img in
-        let x = Random.int (dim_x - patch_size) in
-        let y = Random.int (dim_y - patch_size) in
-        images
-        |> Arr.get_slice [ [ i ]; [ x; x + patch_size - 1 ]; [ y; y + patch_size - 1 ] ]
-        |> fun m -> if Random.bool () then Arr.swap 1 2 m else m)
-    |> Arr.concatenate ~axis:0
+let load_images ?(file = "/home/opam/pkp/pkp-tutorials/data/natural_images.bin") () =
+  Arr.load file
 
 
-module Sparse_coding = struct
-  type prms =
-    { batch_size : int
-    ; n_bases : int
-    ; bfgs_max_iter : int
-    ; lambda : float
-    ; learning_rate : float
-    }
+let get_from_arr x i = x |> Arr.get_slice [ [ i ] ] |> Arr.squeeze
 
-  let default_prms =
-    { batch_size = 100
-    ; n_bases = 100
-    ; bfgs_max_iter = 1000
-    ; lambda = 0.3
-    ; learning_rate = 1.
-    }
-
-
-  (* infer number of pixels *)
-  let n_pix minibatch =
-    let fake = minibatch 1 in
-    let s = Arr.shape fake in
-    assert (s.(1) = s.(2));
-    s.(1) * s.(2)
-
-
-  let cost ~prms ~sigma s =
-    let open Algodiff.D in
-    let s = pack_arr s in
-    let z = float prms.batch_size in
-    fun b a ->
-      let error = Maths.(l2norm_sqr' (s - (b *@ a))) in
-      let sparsity =
-        let h = Maths.(F prms.lambda * log (F 1.0 + sqr (a / F sigma))) in
-        Maths.(sum' h)
-      in
-      Maths.((error + sparsity) / F z)
-
-  (* main loop *)
-  let rec iter ?callback ~prms ~minibatch k a b =
-    Gc.major ();
-    let open Algodiff.D in
-    (* grab the sigma from the previous iteration *)
-    let s =
-      minibatch prms.batch_size
-      |> fun v -> Owl.Arr.reshape v [| prms.batch_size; -1 |] |> Owl.Mat.transpose
-    in
-    let cost = cost ~prms ~sigma:0.05 s in
-    (* optimize the activations given fixed basis functions *)
-    let a =
-      Owl_lbfgs.minimise
-        ~callback:(fun st _ -> Lbfgs.iter st > prms.bfgs_max_iter)
-        (S { f = cost b; init_prms = Mat.zeros Mat.(row_num a) Mat.(col_num a) })
-      |> snd
-      |> Owl_lbfgs.unpack_s
-    in
-    (* take a gradient step for the basis functions *)
-    let rec descent b kk =
-      if kk = 0
-      then b
-      else (
-        let db = (grad (fun b -> cost b a)) b in
-        let b = Maths.(b - (F prms.learning_rate * db)) in
-        descent b (kk - 1))
-    in
-    let b = descent b 1 |> unpack_arr in
-    let b = Owl.Mat.(b / l2norm ~axis:0 b) |> pack_arr in
-    (match callback with
-    | Some f ->
-      let c = cost b a |> unpack_flt in
-      f k (unpack_arr a) (unpack_arr b) c
-    | None -> ());
-    iter ?callback ~prms ~minibatch (k + 1) a b
-
-
-  let optimize ?callback prms minibatch =
-    let n_pix = n_pix minibatch in
-    let a =
-      Mat.gaussian ~sigma:0.08 prms.n_bases prms.batch_size |> Algodiff.D.pack_arr
-    in
-    let b =
-      let b = Mat.gaussian n_pix prms.n_bases in
-      Mat.(b / l2norm ~axis:0 b) |> Algodiff.D.pack_arr
-    in
-    iter ?callback ~prms ~minibatch 1 a b
-end
-
-(** round a float to the nearest integer *)
 let round x =
   let a = floor x
   and b = ceil x in
   int_of_float (if x -. a > b -. x then b else a)
 
 
-let plot_bases ?(display_id = Jupyter_notebook.display "text/html" "") grid_size b =
-  let n_pix, n_bases = Mat.shape b in
-  assert (grid_size * grid_size <= n_bases);
-  let size = n_pix |> float |> sqrt |> round in
-  assert (size * size = n_pix);
+let resize_to_img x =
+  if Arr.num_dims x = 3
+  then x
+  else (
+    let n = Arr.(shape x).(0) in
+    let n_pix = Arr.(shape x).(1) in
+    let size = round (sqrt (float n_pix)) in
+    Arr.reshape x [| n; size; size |])
+
+
+type placeholder = Jupyter_notebook.display_id
+
+let placeholder () = Jupyter_notebook.display "text/html" ""
+
+let plot_image ?ph ?(size = 300, 300) x =
+  let open Gp in
+  let r = max (abs_float (Mat.min' x)) (Mat.max' x) in
+  let figure (module P : Plot) =
+    P.heatmap
+      Mat.(flip ~axis:0 x)
+      [ barebone
+      ; set "size square"
+      ; margins [ `top 1.0; `bottom 0.; `left 0.; `right 1. ]
+      ; set "palette model RGB defined (-1 'black', 1 'white')"
+      ; cbrange (-.r, r)
+      ]
+  in
+  Juplot.draw ?display_id:ph ~size figure
+
+
+type stream = int -> Arr.arr
+
+let create_stream images =
+  let patch_size = 14 in
+  let n_img = Arr.(shape images).(0) in
+  let dim_x = Arr.(shape images).(1) in
+  let dim_y = Arr.(shape images).(2) in
+  fun batch_size ->
+    Array.init batch_size (fun _ ->
+        let i = Random.int n_img in
+        let x = Random.int (dim_x - patch_size) in
+        let y = Random.int (dim_y - patch_size) in
+        Arr.get_slice
+          [ [ i ]; [ x; x + patch_size - 1 ]; [ y; y + patch_size - 1 ] ]
+          images
+        |> fun m -> if Random.bool () then Arr.swap 1 2 m else m)
+    |> Arr.concatenate ~axis:0
+
+
+let sample_stream stream k = stream k
+
+let visualise_stream stream =
+  let display_id = Jupyter_notebook.display "text/html" "" in
+  let rec iter k =
+    if k > 0
+    then (
+      let x = get_from_arr (stream 1) 0 in
+      let open Gp in
+      let figure (module P : Plot) =
+        P.heatmap
+          x
+          [ barebone
+          ; cbrange (-3., 3.)
+          ; margins [ `bottom 0.; `top 1.0; `left 0.; `right 1.0 ]
+          ; set "size square"
+          ; set "palette model RGB defined (-1 'black', 1 'white')"
+          ]
+      in
+      Juplot.draw ~size:(50, 50) ~display_id figure;
+      Unix.sleepf 0.1;
+      iter (k - 1))
+  in
+  iter 100
+
+
+let grid_size n =
+  let rec find k = if k * k >= n then k else find (k + 1) in
+  find 1
+
+
+let plot_patches ?(display_id = Jupyter_notebook.display "text/html" "") p =
+  let n = min Arr.(shape p).(0) 100 in
+  let p = Arr.get_slice [ [ 0; pred n ] ] p in
+  let p = resize_to_img p in
+  let gs = grid_size n in
+  let r = 1.5 *. Stats.quantile (Arr.to_array (Arr.abs p)) 0.95 in
   let open Gp in
   let figure (module P : Plot) =
     P.multiplot
       ~rect:((0.1, 0.1), (0.9, 0.9))
       ~spacing:(0.01, 0.01)
-      (grid_size, grid_size)
+      (gs, gs)
       (fun k _ _ ->
-        let patch = Mat.(reshape (col b k) [| size; size |]) in
-        P.heatmap patch ~style:"image pixels" [ barebone; unset "colorbox" ])
+        if k < n
+        then (
+          let patch = get_from_arr p k in
+          P.heatmap
+            patch
+            [ barebone
+            ; unset "colorbox"
+            ; cbrange (-.r, r)
+            ; set "palette model RGB defined (-1 'blue', 0 'white', 1 'red')"
+            ]))
   in
   Juplot.draw ~fmt:`png ~size:(500, 500) ~display_id figure
 
 
-let default_callback () =
+let default_callback iterations =
   let text_id = Jupyter_notebook.display "text/html" "" in
   let img_id = Jupyter_notebook.display "text/html" "" in
-  let cost_history = ref [] in
-  let log_cost c =
-    cost_history := c :: !cost_history;
-    if List.length !cost_history > 100
-    then cost_history := !cost_history |> List.rev |> List.tl |> List.rev
-  in
-  let mean x = List.fold_left ( +. ) 0. x /. float (List.length x) in
-  fun iter _ b cost ->
-    log_cost cost;
-    Jupyter_notebook.printf
-      "iteration: %05i | cost [avg over last 100 iterations]: %.5f%!"
-      iter
-      (mean !cost_history);
+  fun iter proto ->
+    Jupyter_notebook.printf "iteration: %05i/%i%!" iter iterations;
     Jupyter_notebook.display_formatter ~display_id:text_id "text/html" |> ignore;
-    if iter mod 100 = 0 then plot_bases ~display_id:img_id 10 b
+    if iter mod 10 = 0 then plot_patches ~display_id:img_id proto
+
+
+(* infer number of pixels *)
+let n_pix stream =
+  let fake = get_from_arr (stream 1) 0 in
+  let a, b = Mat.shape fake in
+  a * b
+
+
+(* s: n_img x size x size *)
+let most_likely_intensities cost_fun proto s =
+  let batch_size = Arr.(shape s).(0) in
+  let n_proto = Arr.(shape proto).(0) in
+  let proto = Arr.reshape proto [| n_proto; -1 |] in
+  let open Algodiff.D in
+  (* function to be minimised *)
+  let f = cost_fun s (pack_arr proto) in
+  Owl_lbfgs.minimise
+    ~callback:(fun st _ -> Lbfgs.iter st > 1000)
+    (S { f; init_prms = Mat.gaussian batch_size n_proto })
+  |> snd
+  |> Owl_lbfgs.unpack_s
+  |> unpack_arr
+
+
+(* main optimization loop *)
+let optimise ~n_proto ~batch_size ~iterations ~learning_rate ~cost_fun stream =
+  let callback = default_callback iterations in
+  let n_pix = n_pix stream in
+  let proto =
+    let proto = Mat.gaussian n_proto n_pix in
+    Mat.(proto / l2norm ~axis:1 proto) |> Algodiff.D.pack_arr
+  in
+  let rec iter k proto =
+    if k > iterations
+    then resize_to_img (Algodiff.D.unpack_arr proto)
+    else (
+      Gc.minor ();
+      (* sample image patches from the stream *)
+      let s = stream batch_size in
+      let open Algodiff.D in
+      let n_proto = Arr.(shape proto).(0) in
+      let cost_ = cost_fun s in
+      (* optimise the activations given fixed prototypical templates *)
+      let x =
+        Owl_lbfgs.minimise
+          ~callback:(fun st _ -> Lbfgs.iter st > 1000)
+          (S { f = cost_ proto; init_prms = Mat.zeros batch_size n_proto })
+        |> snd
+        |> Owl_lbfgs.unpack_s
+      in
+      let proto =
+        let dproto = (grad (fun proto -> cost_ proto x)) proto in
+        Maths.(proto - (F learning_rate * dproto)) |> unpack_arr
+      in
+      (* renormalise and perhaps display *)
+      let proto = Owl.Mat.(proto / l2norm ~axis:1 proto) in
+      (* compute cost *)
+      callback k proto;
+      iter (k + 1) (pack_arr proto))
+  in
+  iter 0 proto
+
+
+module Dense_model = struct
+  let cost_fun s =
+    let lambda = 0.3 in
+    let batch_size = Arr.(shape s).(0) in
+    let s = Arr.reshape s [| batch_size; -1 |] in
+    let open Algodiff.D in
+    let s = pack_arr s in
+    fun proto x ->
+      let error = Maths.(l2norm_sqr' (s - (x *@ proto))) in
+      let sparsity = Maths.(F lambda * l2norm_sqr' x) in
+      Maths.((error + sparsity) / F (float batch_size))
+
+
+  let learn ?(k = 25) ?(iterations=1000) ?(learning_rate = 0.02) stream =
+    optimise ~n_proto:k ~batch_size:100 ~iterations ~learning_rate ~cost_fun stream
+
+
+  let most_likely_intensities = most_likely_intensities cost_fun
+end
+
+module Sparse_model = struct
+  (* s : n_img * size * size *)
+  let cost_fun ~lambda s =
+    let batch_size = Arr.(shape s).(0) in
+    let s = Arr.reshape s [| batch_size; -1 |] in
+    let open Algodiff.D in
+    let sigma = 0.05 in
+    let s = pack_arr s in
+    (* proto: n_proto x n_pix
+     x: batch_size x n_proto
+  *)
+    fun proto x ->
+      let error = Maths.(l2norm_sqr' (s - (x *@ proto))) in
+      let sparsity =
+        let h = Maths.(F lambda * log (F 1.0 + sqr (x / F sigma))) in
+        Maths.(sum' h)
+      in
+      Maths.((error + sparsity) / F (float batch_size))
+
+
+  let learn ?(k = 100) ?(iterations=1000) ?(learning_rate = 0.5) stream =
+    let cost_fun = cost_fun ~lambda:0.3 in
+    optimise ~n_proto:k ~batch_size:100 ~iterations ~learning_rate ~cost_fun stream
+
+
+  let most_likely_intensities ?(lambda = 0.01) proto s =
+    most_likely_intensities (cost_fun ~lambda) proto s
+end
+
+let reconstruct proto x =
+  let batch_size, n_proto = Mat.shape x in
+  assert (Arr.num_dims proto = 3 && Arr.(shape proto).(0) = n_proto);
+  let size = Arr.(shape proto).(1) in
+  let proto = Arr.reshape proto [| n_proto; -1 |] in
+  let s = Mat.(x *@ proto) in
+  Mat.reshape s [| batch_size; size; size |]
--- src/visual_coding.mli
+++ src/visual_coding.mli
@@ -1,26 +1,65 @@
-(** Visual coding: dense vs sparse coding *)
-
 open Owl
 
-val minibatch : string -> int -> int -> Arr.arr
+(** {1 Dealing with the data (images)} *)
 
-module Sparse_coding : sig
-  type prms =
-    { batch_size : int
-    ; n_bases : int
-    ; bfgs_max_iter : int
-    ; lambda : float
-    ; learning_rate : float
-    }
+(** Load the image bank from disk *)
+val load_images : ?file:string -> unit -> Arr.arr
 
-  val default_prms : prms
+(** [get_from_arr x i] extracts the [i]^th matrix from 3D array [x] *)
+val get_from_arr : Arr.arr -> int -> Mat.mat
 
-  val optimize
-    :  ?callback:(int -> Mat.mat -> Mat.mat -> float -> unit)
-    -> prms
-    -> (int -> Arr.arr)
-    -> unit
+type placeholder
+
+val placeholder : unit -> placeholder
+
+(** Plot an image in the notebook *)
+val plot_image : ?ph:placeholder -> ?size:int * int -> Mat.mat -> unit
+
+type stream
+
+(** Creates a stream of small image patches from a bank of large images. *)
+val create_stream : Arr.arr -> stream
+
+(** [sample_stream s k] samples [k] image patches from stream [s]. *)
+val sample_stream : stream -> int -> Arr.arr
+
+(** Samples a sequence of image patches from a stream and plots them in the notebook. *)
+val visualise_stream : stream -> unit
+
+(** Plot a bank of image patches on a grid *)
+val plot_patches : ?display_id:Jupyter_notebook.display_id -> Arr.arr -> unit
+
+(** {1 Learning internal models} *)
+
+module Dense_model : sig
+  (** Learn a dense model from a stream of data. Returns the
+      set of prototypical patches that constitute the model. *)
+  val learn : ?k:int -> ?iterations:int -> ?learning_rate:float -> stream -> Arr.arr
+
+  (** [most_likely_intensities proto s] returns the feature intensities that are
+      most likely to have given rise to a particular image [s], given a set
+      [proto] of prototypical features. If there are [N] images packed in [s],
+      and [K] prototypical features, then the result is a [N x K] matrix. *)
+  val most_likely_intensities : Arr.arr -> Arr.arr -> Mat.mat
 end
 
-val plot_bases : ?display_id:Jupyter_notebook.display_id -> int -> Mat.mat -> unit
-val default_callback : unit -> int -> Mat.mat -> Mat.mat -> float -> unit
+module Sparse_model : sig
+  (** Learn a dense model from a stream of data. Returns the
+      set of [k] prototypical patches that constitute the model. *)
+  val learn : ?k:int -> ?iterations:int -> ?learning_rate:float -> stream -> Arr.arr
+
+  (** [most_likely_intensities proto s] returns the feature intensities that are
+      most likely to have given rise to a particular image [s], given a set
+      [proto] of prototypical features. If there are [N] images packed in [s],
+      and [K] prototypical features, then the result is an [N x K] matrix. The
+      optional parameter [lambda] determines how sparse the intensities are
+      assumed to be, a priori. *)
+  val most_likely_intensities : ?lambda:float -> Arr.arr -> Arr.arr -> Mat.mat
+end
+
+(** [reconstruct proto x] reconstructs an image patch as a linear combination of
+    prototypical features given in [proto], weighted by intensities given in [x].
+    If [proto] is a 3D array of dimensions [K x M x M], and [x] is a [N x K]
+    matrix, then the result is a 3D array of dimensions [N x M x M], i.e. [N]
+    images of size [M x M] each. *)
+val reconstruct : Arr.arr -> Mat.mat -> Arr.arr
